49e93d47a59c5163d8f6ff35c14c75fdeb206a67
Vladimír Zdražil <git@vzdrazil.com>
2024-03-16 09:34:14 +0100

    refactor structure

---
+++ src/ExchangeRate.Api/Features/ExchangeRates/Api/ExchangeRateGetAllController.cs
@@ -0,0 +1,56 @@
+using ExchangeRate.Api.Features.CnbIntegration.Models;
+using ExchangeRate.Api.Features.CnbIntegration.Services;
+using ExchangeRate.Api.Features.ExchangeRateApplication.Service;
+using ExchangeRate.Api.Features.ExchangeRates.Api.ContractMapping;
+using ExchangeRate.Api.Features.ExchangeRates.Api.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRate.Api.Features.ExchangeRateApi;
+
+[ApiController]
+public class ExchangeRatesGetAllController : ControllerBase
+{
+    public const string Path = ExchangeRateEndpoints.Base;
+
+    private readonly IExchangeRateService _exchangeRateService;
+    private readonly ICnbIntegrationService _cnbService;
+
+    public ExchangeRatesGetAllController(
+        IExchangeRateService exchangeRateService,
+        ICnbIntegrationService cnbService
+    )
+    {
+        _exchangeRateService = exchangeRateService;
+        _cnbService = cnbService;
+    }
+
+    [HttpGet(Path)]
+    [ProducesResponseType(typeof(ExchangeRateResponse), StatusCodes.Status200OK)]
+    public async Task<ActionResult> GetAll(
+        [FromQuery] GetAllExchangeRatesRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var options = request.MapToOptions();
+
+        var res = await _cnbService.GetDailyExchangeRatesAsync(
+            new GetCnbDailyExchangeRatesOptions { Date = DateTime.Now },
+            cancellationToken
+        );
+
+        var exchangeRates = await _exchangeRateService.GetExchangeRatesAsync(
+            options,
+            cancellationToken
+        );
+
+        var exchangeRatesCount = await _exchangeRateService.GetCountAsync(cancellationToken);
+
+        var response = exchangeRates.MapToResponse(
+            request.Page,
+            request.PageSize,
+            exchangeRatesCount
+        );
+
+        return Ok(response);
+    }
+}
